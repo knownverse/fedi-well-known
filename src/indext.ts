3f82264c9389fbf6d5ca403dd1e00a39838ea96f
Habi <habilis.social@outlook.com>
2024-09-08 20:46:58 +0100

    fix blocklist export

--- src/indext.ts
+++ src/indext.ts
@@ -1,5 +1,7 @@
 import { fetchJSON, WellKnowResult } from "./common";
 import { fetchNodeInfo, fetchWellKnownLinks, fetchWellKnown } from "./fetchNodeInfo";
+import { fetchBlockList } from "./blocklists";
 
 export {fetchNodeInfo, WellKnowResult}
 export {fetchJSON, fetchWellKnownLinks, fetchWellKnown}
+export { fetchBlockList}
